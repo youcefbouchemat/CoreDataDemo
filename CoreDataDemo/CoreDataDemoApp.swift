74a5e090756a135eca0e19bf5f7a57c1eabb7110
YoucefMac <youcefbou1998@gmail.com>
2023-02-27 18:29:16 +0100

    initial commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by apple on 27/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
